ade749a8178f1338834d479fa84d9ec84e2542fd
follpyslipers <109854381+follpyslipers@users.noreply.github.com>
2024-05-25 07:51:58 +0100

    testing 404 page

--- ebook/settings.py
+++ ebook/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lbdqe-r26a87u!is#s#%aa-+e+tt4$kwdqs@!&om0^4-fyjd)j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
 
 ALLOWED_HOSTS = [
     'uniflex.onrender.com',
