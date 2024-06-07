39e7ee3dbaf1ceb214a0af1e23877684d8f11cbd
follpyslipers <109854381+follpyslipers@users.noreply.github.com>
2024-06-07 07:40:41 +0100

    feed admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,19 @@
 # admin.py
 from django.contrib import admin
-from .models import EmailSubscription
+from .models import EmailSubscription,FeedBack
 
 admin.site.register(EmailSubscription)
+
+
+
+class FeedBackAdmin(admin.ModelAdmin):
+    list_display = ('id', 'feedback')
+    search_fields = ('feedback',)
+    readonly_fields = ('id',)
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ('feedback',)
+        return self.readonly_fields
+
+admin.site.register(FeedBack, FeedBackAdmin)
