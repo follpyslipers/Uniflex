d57be62b23dac66b77fb658441ebc1c489d2deb9
follpyslipers <109854381+follpyslipers@users.noreply.github.com>
2024-06-09 11:10:44 +0100

    logo fix

--- static/service-worker.js
+++ static/service-worker.js
@@ -3,8 +3,8 @@ self.addEventListener('install', event => {
         caches.open('v1').then(cache => {
             return cache.addAll([
                 '/',
-                '/static/home/images/logo.png',
-                '/static/home/images/logo.png',
+                '/https://uniabujaflex.s3.amazonaws.com/static/home/images/logo.png',
+                '/https://uniabujaflex.s3.amazonaws.com/static/home/images/logo.png',
                 // Add other assets you want to cache
             ]);
         })
