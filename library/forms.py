e66b9248f4ae84069715719f79303e17bb9818bd
follpyslipers <109854381+follpyslipers@users.noreply.github.com>
2024-06-16 12:27:14 +0100

    s000

--- library/forms.py
+++ library/forms.py
@@ -4,21 +4,12 @@ from .models import E_Book
 class EBookUploadForm(forms.ModelForm):
     class Meta:
         model = E_Book
-        fields = ['title', 'description', 'file']
+        fields = [ 'description', 'file']
 
-    title = forms.CharField(
-        label='Title',
-        widget=forms.TextInput(attrs={'placeholder': 'Enter title'})
-    )
+
 
     description = forms.CharField(
         label='Description',
         required=False,
         widget=forms.Textarea(attrs={'placeholder': 'Enter description (optional)'})
     )
-
-    def clean_title(self):
-        title = self.cleaned_data['title']
-        if len(title) < 3:
-            raise forms.ValidationError("Title must be at least 5 characters long")
-        return title
---
+++ readme.txt
@@ -0,0 +1,6 @@
+...
+sh -->> livesever :: python manage.py runsslserver --certificate cert.pem --key key.pem
+
+
+befor add  to git hub manage.py and setting.py lookup
+ 
